0bf17f4c748e39730590485d296d0986bf4ff995
Andrew Friedman <andrewfriedman01@gmail.com>
2024-03-10 17:59:36 -0400

    Add solution to Values & Variables lecture

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,15 @@
+// Values and Variables
+/*
+1. Declare variables called country, continent 
+and population and assign their values according 
+to your own country (population in millions).
+2. Log their values to the console.
+*/
+let country = "United States of America";
+let continent = "North America";
+let population = 331.9;
+console.log(
+    "Country: " + country + 
+    "\nContinent: " + continent + 
+    "\nPopulation: " + population
+);
