a00ec853684a7e139c96ce570d7caed5b4d72661
Andrew Friedman <andrewfriedman01@gmail.com>
2024-03-11 14:18:23 -0400

    Add solution - let, const, & var

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -5,8 +5,8 @@ and population and assign their values according
 to your own country (population in millions).
 2. Log their values to the console.
 */
-let country = "United States of America";
-let continent = "North America";
+const country = "United States of America";
+const continent = "North America";
 let population = 331.9;
 /*console.log(
     "Country: " + country + 
@@ -23,8 +23,27 @@ language, but don't assign it any value yet.
 2. Log the types of isIsland, population, country 
 and language to the console.
 */
-let isIsland = false
+const isIsland = false
 let language;
+/*console.log(
+    "isIsland type: " + (typeof isIsland) +
+    "\nPopulation type: " + (typeof population) +
+    "\nCountry type: " + (typeof country) + 
+    "\nLanguage type: " + (typeof language) 
+);*/
+
+// let, const and var
+/*
+1. Set the value of language to the language spoken 
+where you live (some countries have multiple languages, 
+but just choose one).
+2. Think about which variables should be const variables 
+(which values will never change, and which might change?). 
+Then, change these variables to const.
+3. Try to change one of the changed variables now, 
+and observe what happens.
+*/
+language = "English";
 console.log(
     "isIsland type: " + (typeof isIsland) +
     "\nPopulation type: " + (typeof population) +
