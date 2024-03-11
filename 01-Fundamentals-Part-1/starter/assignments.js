35595641c56e241866279133357437cee9b52baa
Andrew Friedman <andrewfriedman01@gmail.com>
2024-03-11 14:41:20 -0400

    Add solution - basic operators

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -44,9 +44,35 @@ Then, change these variables to const.
 and observe what happens.
 */
 language = "English";
-console.log(
+/*console.log(
     "isIsland type: " + (typeof isIsland) +
     "\nPopulation type: " + (typeof population) +
     "\nCountry type: " + (typeof country) + 
     "\nLanguage type: " + (typeof language) 
-);
+);*/
+
+// Basic Operators
+/*
+1. If your country split in half, and each half would 
+contain half the population, then how many people 
+would live in each half?
+2. Increase the population of your country by 1 and 
+log the result to the console.
+3. Finland has a population of 6 million. Does your 
+country have more people than Finland?
+4. The average population of a country is 33 million 
+people. Does you country have less people than 
+the average country?
+5. Based on the variables you created, create a new 
+variable description which contains a string with this 
+format: 'Portugal is in Europe, and its 11 million 
+people speak portuguese'.
+*/
+console.log("Half population: " + (population / 2));
+population++;
+console.log("Population: " + population);
+console.log("Is Finland more populated: " + (population < 6));
+console.log("Is average more populated: " + (population < 33));
+const description = country + " is in " + continent +
+    ", and its " + population + " million people speak " + language;
+console.log(description); 
