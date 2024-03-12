00c3b9ce41b689f3036c721f672de825b2451a0d
Andrew Friedman <andrewfriedman01@gmail.com>
2024-03-12 14:41:09 -0400

    Add solution to Taking Decisions: if / else Statements

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -85,4 +85,24 @@ Recreate the description variable from the last assignment,
 this time using the template literal syntax.
 */
 const description = `${country} is in ${continent}, and its ${population} million perople speak ${language}`;
-console.log(description);
+// console.log(description);
+
+// Taking Decisions: if / else Statements
+/*
+1. If your country's population is greater than 33 million,
+log a string like this to the console: "Portugal's population
+is 22 million below average" (the 22 is the average of 33 minus
+the country's population).
+2. After checking the result, change the population temporarily 
+to 13 and then to 130. See the different results, and set the 
+population back to original.
+*/
+
+if (population > 33) {
+    console.log(`${country}'s population is above average`);
+} else {
+    console.log(
+        `${country}'s population is ${33 - population} million
+        below average`
+    );
+}
