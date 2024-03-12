c021ab60fd9fb18dc0fb4d21e2d534bd23bbcf28
Andrew Friedman <andrewfriedman01@gmail.com>
2024-03-12 17:08:17 -0400

    Add solution for conditional ternary operator

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -195,7 +195,7 @@ switch (language) {
         console.log("2nd place in number of place speakers");
         break;
     case("English"):
-        console.log("3rd place");
+        // console.log("3rd place");
         break;
     case ("Hindi"):
         console.log("Number 4");
@@ -206,3 +206,20 @@ switch (language) {
     default:
         console.log("Great language too :D");
 }
+
+// The Conditional (Ternary) Operator
+/*
+1. If your country's population is greater than 33 million, 
+use the ternary operator to log a string like this to the
+console: "Portugal's population is above average". Otherwise, 
+simply log "Portugal's population is below average". Notice
+how only one word change between these two sentences!
+2. After checking the result, change the population temporarily 
+to 13 and then to 130. See the different results, and set the
+population back to original.
+*/
+console.log(
+    `${country}'s population is ${
+        population > 33 ? "above" : "below"
+    } average`
+);
