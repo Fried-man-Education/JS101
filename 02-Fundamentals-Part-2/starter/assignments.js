866a0e2386d56702b3f55e63ce7cd64164daeb3f
Andrew Friedman <andrewfriedman01@gmail.com>
2024-03-14 13:56:09 -0400

    Add solution to functions dec vs expr

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -28,4 +28,38 @@ const uk = describeCountry(
     80,
     "London"
 );
-console.log(usa, brazil, uk);
+// console.log(usa, brazil, uk);
+
+// Function Declarations vs. Expressions
+/*
+1. The world population is 7900 million people. Create 
+a function declaration called percentageOfWorld1 which 
+receives a population value, and returns the percentage 
+of the world population that the given population represents. 
+For example, China has 1441 million people, so it's about 
+18.2% of the world population.
+2. To calculate the percentage, divide the given population 
+value by 7900 and then multiply by 100.
+3. Call percentageOfWorld1 for 3 populations of countries of 
+your choice, store the results into variables, and log them 
+to the console.
+4. Create a function expression which does the exact same 
+thing, called percentageOfWolrd2, and also call it with 3 
+country populations (can be the same populations).
+*/
+function percentageOfWorld1 (population) {
+    return population / 7900 * 100;
+}
+
+let usaPerc = percentageOfWorld1(333);
+let brazilPerc = percentageOfWorld1(100);
+let ukPerc = percentageOfWorld1(80);
+console.log(usaPerc, brazilPerc, ukPerc);
+
+let percentageOfWorld2 = function (population) {
+    return population / 7900 * 100;
+};
+usaPerc = percentageOfWorld2(333);
+brazilPerc = percentageOfWorld2(100);
+ukPerc = percentageOfWorld2(80);
+console.log(usaPerc, brazilPerc, ukPerc);
