8c33717497b7a2f244ebaef5073118df1815f038
Andrew Friedman <andrewfriedman01@gmail.com>
2024-03-16 04:28:36 -0400

    Add solution to developer skills

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,13 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+function printForecast(arr) {
+  let output = "";
+  for (let i = 0; i < arr.length; i++) {
+    output += `${arr[i]} in ${i + 1} days ... `;
+  }
+  return output;
+}
+
+console.log(printForecast([17, 21, 23]));
+console.log(printForecast([12, 5, -5, 0, 4]));
