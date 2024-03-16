c0b865c270eeb233108860d91ebfad76be71d7eb
Andrew Friedman <andrewfriedman01@gmail.com>
2024-03-16 03:55:45 -0400

    Add solution to while loops

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -227,6 +227,19 @@ listOfNeighbours = [
 ];
 for (let i = 0; i < listOfNeighbours.length; i++) {
   for (let j = 0; j < listOfNeighbours[i].length; j++) {
-    console.log(listOfNeighbours[i][j]);
+    // console.log(listOfNeighbours[i][j]);
   }
+}
+
+// The while Loop
+/*
+1. Recreate the challenge from the lecture Looping Arrays, Breaking and Continuing,
+but this time using a while loop (call the array percentages3).
+2. Reflect on what solution you like better for this task: the for loop or the while 
+loop?
+*/
+let i = 0;
+while (i < populations.length) {
+  percentages2.push(percentageOfWorld1(populations[i]));
+  i++;
 }
