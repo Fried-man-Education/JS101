cfe9943670345bf4e1eb9caf122864ee30bde4b8
Andrew Friedman <andrewfriedman01@gmail.com>
2024-03-16 03:27:26 -0400

    Add solution to looping arrays/breaking/continuing

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -191,5 +191,20 @@ a string like this to the console (for numbers 1 to 50): 'Voter number 1
 is currently voting'.
 */
 for (let i = 1; i <= 50; i++) {
-  console.log(`Voter number ${i} is currently voting`);
+  // console.log(`Voter number ${i} is currently voting`);
+}
+
+// Looping Arrays, Breaking and Continuing
+/*
+1. Let's bring back the populations array from a previous assignment.
+2. Use a for loop to compute an array called percentages2 containing 
+the percentages of the world population for the 4 population values. 
+Use the function percentageWOrld1 that you created earlier.
+3. Confirm that percentages2 contains exactly the same values as the 
+percentages array that we created manually in the previous assignment 
+and reflect on how much better this solution is.
+*/
+let percentages2 = [];
+for (let i = 0; i < populations.length; i++) {
+  percentages2.push(percentageOfWorld1(populations[i]));
 }
